d03ad098a1710063f27e70414b5dd0f131a7c971
samaresh231 <47944092+samaresh231@users.noreply.github.com>
2021-05-18 20:08:37 +0530

    complete project kbc

--- kbc.py
+++ kbc.py
@@ -11,7 +11,7 @@ def isAnswerCorrect(question, answer):
         False if the answer is incorrect
     '''
 
-    return True if answer == 2 else False      #remove this
+    return True if question["answer"] == answer else False
 
 
 def lifeLine(ques):
@@ -20,7 +20,14 @@ def lifeLine(ques):
     :param ques: The question for which the lifeline is asked for. (Type JSON)
     :return: delete the key for two incorrect options and return the new ques value. (Type JSON)
     '''
+    ans = ques["answer"]
 
+    for i in range(1,5):
+        if ques["answer"] != i:
+            print(f'\t\t\tOption {i}: {ques[f"option{i}"]}')
+            break
+    print(f'\t\t\tOption {ans}: {ques[f"option{ans}"]}')
+            
 
 def kbc():
     '''
@@ -47,28 +54,67 @@ def kbc():
     #  Display a welcome message only once to the user at the start of the game.
     #  For each question, display the prize won until now and available life line.
     # For now, the below code works for only one question without LIFE-LINE and QUIT checks
+    has_won = True
+    life_lines_used = False
+    prize_won_till_now = 0
+    questions_answered = 0
+    print('Welcome to the game \nPress any key to continue')
+    input()
 
-    print(f'\tQuestion 1: {QUESTIONS[0]["name"]}' )
-    print(f'\t\tOptions:')
-    print(f'\t\t\tOption 1: {QUESTIONS[0]["option1"]}')
-    print(f'\t\t\tOption 2: {QUESTIONS[0]["option2"]}')
-    print(f'\t\t\tOption 3: {QUESTIONS[0]["option3"]}')
-    print(f'\t\t\tOption 4: {QUESTIONS[0]["option4"]}')
-    ans = input('Your choice ( 1-4 ) : ')
-
-    # check for the input validations
-
-    if isAnswerCorrect(QUESTIONS[0], int(ans) ):
-        # print the total money won.
-        # See if the user has crossed a level, print that if yes
-        print('\nCorrect !')
-
+    if questions_answered < 14 and not life_lines_used:
+        print("0", end = " ")
     else:
-        # end the game now.
-        # also print the correct answer
-        print('\nIncorrect !')
+        life_lines_used = True
+        print("1", end = " ")
+    print("life lines used\n")
+
+    for question in QUESTIONS:
+        print(f'\tQuestion {questions_answered + 1}: {question["name"]}')
+        print(f'\t\tOptions:')
+        print(f'\t\t\tOption 1: {question["option1"]}')
+        print(f'\t\t\tOption 2: {question["option2"]}')
+        print(f'\t\t\tOption 3: {question["option3"]}')
+        print(f'\t\t\tOption 4: {question["option4"]}')
+        ans = input('Your choice ( 1-4 ) : ')
+
+        # check for the input validations
+        if ans.lower() == "quit":
+            break
+        elif ans.lower() == "ll":
+            lifeLine(question)
+            ans = input('Your choice ( 1-4 ) : ')
+        elif int(ans) < 1 and int(ans) > 4:
+            print("Invalid option \nPlease try again")
+            continue
+
+        if isAnswerCorrect(question, int(ans) ):
+            # print the total money won.
+            prize_won_till_now += question["money"]
+            print("Money won till now {} ".format(prize_won_till_now))
+            # See if the user has crossed a level, print that if yes
+            questions_answered += 1 
+            if questions_answered % 5 == 0:
+                print("You have crossed a padaav")
+            print('\nCorrect !')
+
+        else:
+            # end the game now.
+            # also print the correct answer
+            has_won = False
+            print('\nIncorrect ! \nThe correct answer was {}'.format(
+                question['option{}'.format(
+                    question['answer'])]))
+            break
 
     # print the total money won in the end.
-
-
+    print("You have won ", end="")
+    if has_won:
+        print(prize_won_till_now)
+    else:
+        if questions_answered < 5:
+            print("0")
+        elif questions_answered <10:
+            print("10,000")
+        else:
+            print("3,20,000")
 kbc()
